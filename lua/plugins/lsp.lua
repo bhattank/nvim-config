c758e6120b94030ed14ff1d26c974bc855773241
bhattank <bhattankit2000@gmail.com>
2026-03-19 01:45:55 +0000

    added go auto format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,6 +24,15 @@ return {
             "clangd",
 	    "lua_ls",
         })
-    end,
+
+	-- Format on save for Go files 
+	vim.api.nvim_create_autocmd("BufWritePre", {
+	    pattern = "*.go", 
+	    callback = function ()
+		vim.lsp.buf.format({ async = false })
+	    end, 
+	})
+    end, 
 }
+
 
